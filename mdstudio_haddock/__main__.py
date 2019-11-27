d3bf57dec9a7d692e127ec2b6c8637e735dd96e0
Marc van Dijk <marcvdijk@gmail.com>
2019-11-27 12:13:46 +0100

    Dynamically add package to path if needed

--- mdstudio_haddock/__main__.py
+++ mdstudio_haddock/__main__.py
@@ -1,5 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
 from mdstudio.runner import main
-from mdstudio_haddock.haddock_wamp_endpoints import HaddockComponent
+
+# Try import package
+try:
+    from mdstudio_haddock.haddock_wamp_endpoints import HaddockComponent
+
+except ImportError:
+
+    # Add modules in package to path
+    modulepath = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+    if modulepath not in sys.path:
+        sys.path.insert(0, modulepath)
+
+    from mdstudio_haddock.haddock_wamp_endpoints import HaddockComponent
 
 
 if __name__ == '__main__':
